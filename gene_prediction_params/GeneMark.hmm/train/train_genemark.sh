ce57266c3d51f3aa21212868c70a1cd5ee4e53c1
Sawyer Masonjones <smaso003@ucr.edu>
2016-04-15 14:20:01 -0700

    genemark training run

--- gene_prediction_params/GeneMark.hmm/train/train_genemark.sh
+++ gene_prediction_params/GeneMark.hmm/train/train_genemark.sh
@@ -1,5 +1,5 @@
-#PBS -q batch -l nodes=1:ppn=1,walltime=192:00:00,mem=8gb -j oe
-module load genemarkHMM/2.3e
+#PBS -q batch -l nodes=1:ppn=8,walltime=192:00:00,mem=4gb -j oe
+module load genemarkHMM/4.32
 
 SPECIES=Chaetosphaeria
 CPU=1
@@ -7,6 +7,7 @@ if [ $PBS_PPN ]; then
  CPU=$PBS_PPN
 fi
 if [ ! -f $SPECIES.long.fasta ]; then
+ # from https://github.com/hyphaltip/genome-scripts
  perl /rhome/jstajich/src/genome-scripts/gene_prediction/select_long_ctgs.pl --min 10000 $SPECIES.fa > $SPECIES.long.fasta
 fi
-nohup gm_es.pl $SPECIES.long.fasta >& train.log
+gmes_petap.pl --cores $CPU --ES --training --fungus --sequence $SPECIES.long.fasta
